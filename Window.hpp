de8191501b53f68376cc98429b58526c2640ca08
Roland <roland_zwart@outlook.com>
2020-10-20 12:58:20 +0200

    texture mapping, singletons removed, lnk2005 fixed

--- Window.hpp
+++ Window.hpp
@@ -3,32 +3,19 @@
 #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
 
-struct WindowSpecs
-{
-    uint32_t width;
-    uint32_t height;
-};
-
 class Window
 {
-protected:
-    Window() { }
-
-    static Window* window_;
 public:
-    Window(Window& other) = delete;
-    void operator=(const Window&) = delete;
-
-    static Window* GetInstance();
+    bool framebufferResized = false;
 
     void initWindow();
     void cleanup();
-    GLFWwindow* getMemAddres();
-    struct WindowSpecs getSpecs();
+    GLFWwindow* get();
+    static void framebufferResizeCallback(GLFWwindow* window, int width, int height);
 
 private:
-    const uint32_t WIDTH = 800;
-    const uint32_t HEIGHT = 600;
+    const uint32_t WIDTH =1280;
+    const uint32_t HEIGHT = 720;
     
     GLFWwindow* window;
 };
